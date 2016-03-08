8f7998c0e50050e496ecc40f7d54cadcb3238ba7
ryanb <ryanbradford333@gmail.com>
2016-03-08 13:01:02 -0500

    Initial Commit

---
+++ MHSStats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MHSStats
+//
+//  Created by Ryan on 3/8/16.
+//  Copyright © 2016 ryanb3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
