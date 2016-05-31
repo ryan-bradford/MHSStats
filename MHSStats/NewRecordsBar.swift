7c5241cc91ebf99eb32f370bce1b13524f2df1a5
ryanb <ryanbradford333@gmail.com>
2016-05-31 08:12:51 -0400

    Made the new records bar more clear.

--- MHSStats/NewRecordsBar.swift
+++ MHSStats/NewRecordsBar.swift
@@ -22,12 +22,47 @@ public class NewRecordsBar: UIScrollView {
             newRecordButtons.append(toAdd)
             addSubview(toAdd)
         }
-        let neededWidth = FileStructure.newRecordButtonGap / 2.0 + (Double(newRecords.count) - 1.0) * FileStructure.newRecordsBarHeight
+        let neededWidth = FileStructure.newRecordButtonGap + (Double(newRecords.count)) * FileStructure.newRecordsBarHeight
         self.contentSize = (CGSizeMake(CGFloat(neededWidth), CGFloat(FileStructure.newRecordsBarHeight)))
         self.autoresizingMask = UIViewAutoresizing.FlexibleWidth
+        self.backgroundColor = UIColor.clearColor()
     }
     
     required public init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
     }
+    override public func drawRect(rect: CGRect) {
+        if(newRecords!.count == 0) {
+            drawCenteredTextInRect(0, y: 0, width: CGFloat(rect.width), height: CGFloat(FileStructure.newRecordsBarHeight), toDraw: "No New Records", fontSize: 20)
+        }
+    }
+    
+    func drawCenteredTextInRect(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, toDraw: String, fontSize: CGFloat) -> CGFloat {
+        let message: NSMutableAttributedString = NSMutableAttributedString(string: toDraw)
+        
+        let fieldColor: UIColor = UIColor.blackColor()
+        let fieldFont = UIFont(name: "Helvetica Neue", size: CGFloat(fontSize))
+        let paraStyle = NSMutableParagraphStyle()
+        paraStyle.alignment = NSTextAlignment.Center
+        
+        let attributes: NSDictionary = [
+            NSForegroundColorAttributeName: fieldColor,
+            NSParagraphStyleAttributeName: paraStyle,
+            NSFontAttributeName: fieldFont!
+        ]
+        let countString = (message.length)
+        message.addAttributes(attributes as! [String : AnyObject], range: NSRange(location: 0, length: countString) )
+        let textHeight = self.heightWithConstrainedWidth(self.frame.width, font: UIFont.boldSystemFontOfSize(25), toGet: toDraw)
+        message.drawInRect(CGRectMake(x, height / 2 - textHeight / 2 + y, width, height))
+        return textHeight
+    }
+    
+    func heightWithConstrainedWidth(width: CGFloat, font: UIFont, toGet: String) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: CGFloat.max)
+        
+        let boundingBox = toGet.boundingRectWithSize(constraintRect, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return boundingBox.height
+    }
 }
+
